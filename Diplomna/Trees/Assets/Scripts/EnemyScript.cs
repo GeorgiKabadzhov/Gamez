595506ec1e8a789c8a32e3ccb787168a7382ac5d
RoshHoul <leprik@abv.bg>
2015-02-09 20:14:27 +0200

    AI

--- Diplomna/Trees/Assets/Scripts/EnemyScript.cs
+++ Diplomna/Trees/Assets/Scripts/EnemyScript.cs
@@ -1,44 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
-
 public class EnemyScript : MonoBehaviour {
-
-	public float velocity = 1f;
-
+	private Vector3 Player;
+	private Vector2 PlayerDirection;
+	private float Xdif;
+	private float Ydif;
+	public float speed;
+	private int Wall;
+	public float distance;
+	public int Health;
+	public int SpikeyGiveDmg = 40;
+	public bool colliding;
 	public Transform sightStart;
 	public Transform sightEnd;
+	void Start (){
 
-	public bool colliding;
-
-	void Start () {
-	
 	}
-	
-	// Update is called once per frame
+	public void OnTriggerEnter2D(Collider2D other ){
+		if (other.tag == "Player"){
+			PlayerHealth.curHealth -= SpikeyGiveDmg;
+		}
+	}
 	void Update () {
 		int direction;
 
-		rigidbody.velocity = new Vector2 (velocity, rigidbody.velocity.y); 
+		rigidbody.velocity = new Vector2 (speed,rigidbody.velocity.y);
 		colliding = Physics.Linecast (sightStart.position, sightEnd.position);
-
-
-		if (colliding) {
+		
+		
+	/*	if (colliding) {
 			transform.localScale = new Vector2 ( transform.localScale.x * -1, transform.localScale.y);
-			velocity*= -1;
-		}
-	}
+			speed*= -1;
+		} */
 
-	void OnDrawGizmos() {
-		Gizmos.color = Color.magenta;
-
-		Gizmos.DrawLine (sightStart.position, sightEnd.position);
-	}
-
-	void OnCollisionEnter(Collision coll) {
-		if (coll.gameObject.tag == "Player") {
-			PlayerHealth.curHealth -= 10; 
-			if (PlayerHealth.curHealth == 0) {
-				Destroy (coll.gameObject);
+		distance = Vector2.Distance (Player, transform.position);
+		Player = GameObject.Find("Trees").transform.position;
+		if (distance < 10) {
+			Xdif = Player.x - transform.position.x;
+			Ydif = Player.y - transform.position.y;
+			PlayerDirection = new Vector2 (Xdif, Ydif);
+			if (!Physics2D.Raycast (transform.position, PlayerDirection, 5, Wall)) {
+				rigidbody.AddForce (PlayerDirection.normalized * speed);
 			}
 		}
 	}
